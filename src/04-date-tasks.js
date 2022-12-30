b5fb47b9b11725a03a27ac14aa1d8537ff2c4a12
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2022-12-30 18:57:18 +0200

    feat: add solutions for simple date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  date.setDate(29);
+  return date.getDate() !== 1;
 }
 
 
@@ -73,8 +74,21 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const delta = endDate - startDate;
+  const hours = Math.floor(delta / (60 * 60 * 1000));
+  const minutes = Math.floor((hours > 0 ? (delta % (hours * 60 * 60 * 1000)) : delta)
+   / (60 * 1000));
+  const seconds = Math.floor((delta - (hours * 60 * 60 * 1000) - (minutes * 60 * 1000)) / 1000);
+  const mc = delta - (hours * 60 * 60 * 1000) - (minutes * 60 * 1000) - (seconds * 1000);
+
+  let temp = (hours < 10 ? '0' : '') + hours;
+  temp += (minutes < 10 ? ':0' : ':') + minutes;
+  temp += (seconds < 10 ? ':0' : ':') + seconds;
+  // eslint-disable-next-line no-nested-ternary
+  temp += (mc < 10 ? '.00' : (mc < 100 && mc >= 10) ? '.0' : '.') + mc;
+
+  return temp;
 }
 
 
